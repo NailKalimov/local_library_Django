1b27edbad52fe9f3f3ad4d58e1c8263d5bc37c22
nailkalimov <nailkalimov@gmail.com>
2025-01-03 22:56:14 +0400

    chapter 9

--- locallibrary/catalog/urls.py
+++ locallibrary/catalog/urls.py
@@ -9,4 +9,8 @@ urlpatterns = [
     path(r'^author/(?P<pk>\d+)$', view=views.AuthorDetailView.as_view(), name='author_detail'),
     path(r'^mybooks/$', views.LoanedBooksByUserListView.as_view(), name='my-borrowed'),
     path('librarer/', views.AllLoanedBooksForStaff.as_view(), name='librarer-page'),
+    path(r'^book/(?<pk>[-\w]+/renew/$)', views.renew_book_librarian, name='renew-book-librarian'),
+    path(r'author/create/$', views.AuthorCreate.as_view(), name='author_create'),
+    path(r'^author/(?P<pk>\d+)/update/$', views.AuthorUpdate.as_view(), name='author_update'),
+    path(r'^author/(?P<pk>\d+)/delete/$', views.AuthorDelete.as_view(), name='author_delete'),
 ]
--- locallibrary/catalog/views.py
+++ locallibrary/catalog/views.py
@@ -1,7 +1,15 @@
+import datetime
+from django.contrib.auth.decorators import permission_required
 from django.contrib.auth.mixins import LoginRequiredMixin, PermissionRequiredMixin
-from django.shortcuts import render
+from django.http import HttpResponseRedirect
+from django.shortcuts import render, get_object_or_404
+from django.urls import reverse
+from .forms import RenewBookForm
 from .models import Author, Book, BookInstance
 from django.views import generic
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from django.urls import reverse_lazy
+from .models import Author
 
 
 def index(request):
@@ -57,9 +65,44 @@ class LoanedBooksByUserListView(LoginRequiredMixin, generic.ListView):
     def get_queryset(self):
         return BookInstance.objects.filter(borrower=self.request.user).filter(status__exact='o').order_by('due_back')
 
+
 class AllLoanedBooksForStaff(PermissionRequiredMixin, generic.ListView):
     model = BookInstance
     template_name = 'catalog/bookinstance_list_on_loan_for_staff.html'
     permission_required = 'catalog.can_view_all_book_instance_on_loan'
 
 
+@permission_required('catalog.can_view_all_book_instance_on_loan')
+def renew_book_librarian(request, pk):
+    book_inst = get_object_or_404(BookInstance, pk=pk)
+
+    if request.method == 'POST':
+        form = RenewBookForm(request.POST)
+        if form.is_valid():
+            book_inst.due_back = form.cleaned_data['renewal_date']
+            book_inst.save()
+            return HttpResponseRedirect(reverse('librarer-page'))
+
+        return render(request, 'catalog/book_renew_librarian.html',
+                      {'form': form, 'book_inst': book_inst})
+    else:
+        default_renewal_date = datetime.date.today() + datetime.timedelta(weeks=3)
+        form = RenewBookForm(initial={'renewal_date': default_renewal_date})
+        return render(request, 'catalog/book_renew_librarian.html',
+                      {'form': form, 'bookinst': book_inst})
+
+
+class AuthorCreate(CreateView):
+    model = Author
+    fields = '__all__'
+    initial = {'date_of_death': '12/10/2016', }
+
+
+class AuthorUpdate(UpdateView):
+    model = Author
+    fields = ['first_name', 'last_name', 'date_of_birth', 'date_of_death']
+
+
+class AuthorDelete(DeleteView):
+    model = Author
+    success_url = reverse_lazy('authors')
